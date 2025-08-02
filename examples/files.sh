babf2ddc9291f2af50220442fca60df2eea8fd8c
Scott Lusk <scottluskcis@github.com>
2025-08-02 17:47:13 -0400

    Add script for basic file operations in shell
    This script demonstrates basic file operations in a shell.

---
+++ examples/files.sh
@@ -0,0 +1,27 @@
+# 1. Create a new folder
+mkdir demo_folder
+cd demo_folder
+
+# 2. Create a new file
+touch hello.txt
+
+# 3. Write to the file
+echo "Hello, Computer Science students!" > hello.txt
+
+# 4. Rename the file
+mv hello.txt greeting.txt
+
+# 5. Open the file (in default editor, like TextEdit)
+open greeting.txt
+
+# (Optional) Wait for the student to see it open, then continue...
+
+# 6. Read the file content in terminal
+cat greeting.txt
+
+# 7. Delete the file
+rm greeting.txt
+
+# 8. Go up one directory and delete the folder
+cd ..
+rmdir demo_folder
